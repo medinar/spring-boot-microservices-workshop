a0c6e2d1b861ae230c9b43bf6af19449e97d7bf8
rommelmedina <rommelmedina@192.168.100.4>
2020-03-21 18:09:08 +0800

    Using RestTemplate to call External API.

---
+++ movie-catalog-service/src/main/java/com/medinar/spring/moviecatalogservice/model/Movie.java
@@ -0,0 +1,36 @@
+package com.medinar.spring.moviecatalogservice.model;
+
+/**
+ *
+ * @author rommelmedina
+ */
+public class Movie {
+
+    private String movieId;
+    private String name;
+
+    public Movie() {
+    }
+
+    public Movie(String movieId, String name) {
+        this.movieId = movieId;
+        this.name = name;
+    }
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
--- movie-catalog-service/src/main/java/com/medinar/spring/moviecatalogservice/resource/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/medinar/spring/moviecatalogservice/resource/MovieCatalogResource.java
@@ -1,11 +1,17 @@
 package com.medinar.spring.moviecatalogservice.resource;
 
 import com.medinar.spring.moviecatalogservice.model.CatalogItem;
+import com.medinar.spring.moviecatalogservice.model.Movie;
+import com.medinar.spring.moviecatalogservice.model.Rating;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 /**
  *
@@ -14,11 +20,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
+
+    @Autowired
+    RestTemplate restTemplate;
     
     @RequestMapping("/{userId}")
-    public List<CatalogItem> getCatalogItems(@PathVariable("userId") String userId) {      
-        return Collections.singletonList(
-                new CatalogItem("Transformers", "Transformers Desc", 4)
+    public List<CatalogItem> getCatalogItems(@PathVariable("userId") String userId) {
+
+        // get all rated movie IDs
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 4),
+                new Rating("5678", 4)
         );
+
+        return ratings.stream().map(rating -> {
+            Movie movie = restTemplate.getForObject(
+                    "http://localhost:9002/movies/" + rating.getMovieId(),
+                    Movie.class
+            );
+
+            return new CatalogItem(movie.getName(), "Desc", rating.getRating());
+        }).collect(Collectors.toList());
+
+        // For each movie ID, call movie info service and get details
+        // Put them all together
     }
 }

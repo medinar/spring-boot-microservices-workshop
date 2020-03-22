e4987540932b391e38dee6526527ad0bc82a611c
rommelmedina <rommelmedina@192.168.100.4>
2020-03-22 09:37:02 +0800

    Completion state commit: 10. Converted services to Eureka clients.

--- discovery-server/src/main/java/com/medinar/spring/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/medinar/spring/discoveryserver/DiscoveryServerApplication.java
@@ -8,8 +8,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class DiscoveryServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DiscoveryServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServerApplication.class, args);
+    }
 
 }
--- ratings-data-service/src/main/java/com/medinar/spring/ratingsdataservice/RatingsDataServiceApplication.java
+++ ratings-data-service/src/main/java/com/medinar/spring/ratingsdataservice/RatingsDataServiceApplication.java
@@ -2,7 +2,9 @@ package com.medinar.spring.ratingsdataservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class RatingsDataServiceApplication {
 

ed8a6bf4598e2305c60a2107c9018d62231529da
rommelmedina <rommelmedina@192.168.100.4>
2020-03-22 09:09:00 +0800

    Created a Discovery server using Eureka.

---
+++ discovery-server/src/test/java/com/medinar/spring/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medinar.spring.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

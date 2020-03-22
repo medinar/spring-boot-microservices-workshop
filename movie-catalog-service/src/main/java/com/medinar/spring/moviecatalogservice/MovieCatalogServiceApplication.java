b85918d20ef7ede0a3777e941422a0280eb8d315
rommelmedina <rommelmedina@192.168.100.4>
2020-03-22 16:56:06 +0800

    Added a timeout in the movie-catalog-service's RestTemplate bean, to solve the issue on slow external APIs. But this only partially solved the issue.

--- movie-catalog-service/src/main/java/com/medinar/spring/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/medinar/spring/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @EnableEurekaClient
@@ -14,7 +15,9 @@ public class MovieCatalogServiceApplication {
     @LoadBalanced
     @Bean
     public RestTemplate getRestTemplate() {
-        return new RestTemplate();
+        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+        clientHttpRequestFactory.setConnectTimeout(3000);
+        return new RestTemplate(clientHttpRequestFactory);
     }
     
     public static void main(String[] args) {

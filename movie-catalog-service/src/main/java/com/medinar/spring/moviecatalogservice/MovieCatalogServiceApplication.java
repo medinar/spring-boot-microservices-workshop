d8baf521810f899254bbedff64ff48919bab2a20
rommelmedina <rommelmedina@192.168.100.4>
2020-03-22 10:02:18 +0800

    Completion state commit: 11. Discovering services through Eureka

--- movie-catalog-service/src/main/java/com/medinar/spring/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/medinar/spring/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,6 +2,7 @@ package com.medinar.spring.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -10,15 +11,11 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class MovieCatalogServiceApplication {
 
+    @LoadBalanced
     @Bean
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
-
-//    @Bean
-//    public WebClient.Builder getWebClientBuilder() {
-//        return WebClient.builder();
-//    }
     
     public static void main(String[] args) {
         SpringApplication.run(MovieCatalogServiceApplication.class, args);
--- movie-catalog-service/src/main/java/com/medinar/spring/moviecatalogservice/resource/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/medinar/spring/moviecatalogservice/resource/MovieCatalogResource.java
@@ -2,9 +2,7 @@ package com.medinar.spring.moviecatalogservice.resource;
 
 import com.medinar.spring.moviecatalogservice.model.CatalogItem;
 import com.medinar.spring.moviecatalogservice.model.Movie;
-import com.medinar.spring.moviecatalogservice.model.Rating;
 import com.medinar.spring.moviecatalogservice.model.UserRating;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,21 +22,18 @@ public class MovieCatalogResource {
     @Autowired
     RestTemplate restTemplate;
 
-//    @Autowired
-//    WebClient.Builder webClientBuilder;
-
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalogItems(@PathVariable("userId") String userId) {
 
         UserRating userRating = restTemplate.getForObject(
-                "http://localhost:9003/ratings/users/" + userId, 
+                "http://ratings-data-service/ratings/users/" + userId, 
                 UserRating.class
         );
 
         return userRating.getUserRatings().stream().map(rating -> {
             // For each movie ID, call movie info service and get details
             Movie movie = restTemplate.getForObject(
-                    "http://localhost:9002/movies/" + rating.getMovieId(),
+                    "http://movie-info-service/movies/" + rating.getMovieId(),
                     Movie.class
             );
 
@@ -48,10 +43,3 @@ public class MovieCatalogResource {
         
     }
 }
-
-//            Movie movie = webClientBuilder.build()
-//                    .get()
-//                    .uri("http://localhost:9002/movies/" + rating.getMovieId())
-//                    .retrieve()
-//                    .bodyToMono(Movie.class)
-//                    .block();

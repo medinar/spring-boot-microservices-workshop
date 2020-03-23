637c13f1d520ce399ee5714a8a7e4f4038fa101e
rommelmedina <rommelmedina@192.168.100.4>
2020-03-23 15:48:53 +0800

    Implemented Bulkhead pattern using Hystrix.

--- movie-catalog-service/src/main/java/com/medinar/spring/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/com/medinar/spring/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -5,10 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+@EnableHystrixDashboard
 @EnableCircuitBreaker
 @EnableEurekaClient
 @SpringBootApplication
--- movie-catalog-service/src/main/java/com/medinar/spring/moviecatalogservice/service/MovieInfoService.java
+++ movie-catalog-service/src/main/java/com/medinar/spring/moviecatalogservice/service/MovieInfoService.java
@@ -4,6 +4,7 @@ import com.medinar.spring.moviecatalogservice.model.CatalogItem;
 import com.medinar.spring.moviecatalogservice.model.Movie;
 import com.medinar.spring.moviecatalogservice.model.Rating;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -18,7 +19,14 @@ public class MovieInfoService {
     @Autowired
     RestTemplate restTemplate;
 
-    @HystrixCommand(fallbackMethod = "getFallbackCatalogItem")
+    @HystrixCommand(
+            fallbackMethod = "getFallbackCatalogItem",
+            threadPoolKey = "movieInfoPool",
+            threadPoolProperties = {
+                @HystrixProperty(name = "coreSize", value = "20"),
+                @HystrixProperty(name = "maxQueueSize", value = "10")
+            }
+    )
     public CatalogItem getCatalogItem(Rating rating) {
         Movie movie = restTemplate.getForObject(
                 "http://movie-info-service/movies/" + rating.getMovieId(),

129754043b237d2063e048610677565468201404
Mhel Medina <rommel.d.medina@gmail.com>
2020-03-24 11:02:48 +0800

    Setup a Spring Cloud Config Server.

---
+++ spring-cloud-config-server/src/test/java/com/medinar/spring/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medinar.spring.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

aa297cb6b267716575b296c601df2b9c9c608c86
Mhel Medina <rommel.d.medina@gmail.com>
2020-03-24 15:01:40 +0800

    Implemented dynamic configuration.

--- spring-boot-config/src/main/java/com/medinar/spring/springbootconfig/GreetingController.java
+++ spring-boot-config/src/main/java/com/medinar/spring/springbootconfig/GreetingController.java
@@ -3,6 +3,7 @@ package com.medinar.spring.springbootconfig;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author rommelmedina
  */
+@RefreshScope
 @RestController
 public class GreetingController {
 
